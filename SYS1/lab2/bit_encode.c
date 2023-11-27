2fd461d0dd0aed76929f51014fd5e18064677956
Ace Baum <jaybaum.03@gmail.com>
2023-11-27 17:53:44 -0500

    read in message from user

--- SYS1/lab2/bit_encode.c
+++ SYS1/lab2/bit_encode.c
@@ -3,14 +3,23 @@
 
 #define MAX_MESSAGE_LENGTH 200
 
+void get_message(char* );
+
 int main() {
 	/* Variable definitions */ 
+	char *message_array;
+
+	/* Allocate */ 
+	message_array = calloc(MAX_MESSAGE_LENGTH, sizeof(char));
 
 	/* Prompt the user to enter the text to be encrypted */
+	printf("Enter the message to be encrypted: ");
 	
 	/* Use getchar() to reach each ASCII character and store into array */
+	get_message(message_array);
 
 	/* Print out received clear text */
+	printf("Message to encrypt: %s", message_array);
 
 	/* Print out the clear text as hexadecimal numbers */ 
 
@@ -24,5 +33,24 @@ int main() {
 
 	/* Print out the cipher with 10 values per row */ 
 
+	/* Free */
+
+	/* New line for console */ 
+	printf("\n");
+
 	return EXIT_SUCCESS;
+}
+
+void get_message(char* output_arr) {
+	char input_char;
+	int chars_read = 0;
+
+	/* Collect all characters from the input */ 
+	while (input_char = getchar(), input_char != '\n' && chars_read < MAX_MESSAGE_LENGTH) {
+		*(output_arr+chars_read) = input_char;	
+		chars_read++;
+	}
+
+	/* Add null terminating char */ 
+	*(output_arr+chars_read) = '\0';
 }
