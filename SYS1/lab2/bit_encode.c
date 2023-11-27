8991767903e4aa05f5665f5b829d61fb793c9892
Ace Baum <jaybaum.03@gmail.com>
2023-11-27 17:35:10 -0500

    created skeleton from lab readme file

---
+++ SYS1/lab2/bit_encode.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_MESSAGE_LENGTH 200
+
+int main() {
+	/* Variable definitions */ 
+
+	/* Prompt the user to enter the text to be encrypted */
+	
+	/* Use getchar() to reach each ASCII character and store into array */
+
+	/* Print out received clear text */
+
+	/* Print out the clear text as hexadecimal numbers */ 
+
+	/* Prompt the user for a 4 bit key */ 
+
+	/* Use getchar() to read in the 4 digits */ 
+
+	/* Convert key from 4 bits to 8 bits */ 
+
+	/* Cipher the text */ 
+
+	/* Print out the cipher with 10 values per row */ 
+
+	return EXIT_SUCCESS;
+}
